e23dd9523cc0d3269005ace148f36c16ceed1fcc
Sam Thabet <sallam.thabet@gmail.com>
2023-03-04 00:51:13 -0500

    added api functionality

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,15 +1,39 @@
-import { Card, Button,Form } from "react-bootstrap";
+import { Card, Button, Form } from "react-bootstrap";
 import "./App.css";
-
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-function App() {
+import { useState, useEffect } from "react";
+
+async function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+    console.log("latitude is :", lat);
+    console.log("longitude is :", long);
+  });
+
+  await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+    .then(res => res.json())
+    .then(result => {
+      setData(result)
+      console.log(result);
+    });
+
+  
   return (
     <div className="cardBody">
       <Card>
         <Card.Body className="cardBody2">
           <Card.Title style={{ color: "white" }}>Todays Weather</Card.Title>
           <Card.Text style={{ color: "white" }}>
-            Please insert the city to which you would like to check the weather for below.
+            Please insert the city to which you would like to check the weather
+            for below.
           </Card.Text>
           <Form.Control
             type="text"
